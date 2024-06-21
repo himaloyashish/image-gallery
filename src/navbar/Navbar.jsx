28aacb23ee0b25011950cdc9b81fab3c62df09f4
himaloyash <himaloyashis98@gmail.com>
2024-06-21 19:37:10 +0600

    all done in bg of banner and second section

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="flex items-center justify-between pl-[120px] bg-black pt-5  pr-[120px]  ">
       <div>
         <li>
-          <img src="/src/assets/massart 1.png" alt="MassArt" />{" "}
+          <img src="/src/assets/massart 1.png" alt="MassArt" />
         </li>
       </div>
       <div className="flex gap-3">
