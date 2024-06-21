7ef819f90714c73ce653f091ccce7b8a33e690a3
himaloyash <himaloyashis98@gmail.com>
2024-06-21 19:22:43 +0600

    banner done

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -3,8 +3,11 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className=' bannerBg '>
-            <p className='  '>Witness The World’s Biggest</p>
+        <div className=' bannerBg  pl-[120px] pr-[120px]   '>
+            <div className='w-[320px]  pt-[300px] '>
+                <p className=' text-4xl text-white text-start '>Witness The World’s Biggest</p>
+                <p className='text-white text-start text-sm '>durga puja is technically west bengal’s largest religious festival, centred on the goddess durga, destroyer of the demon mahishasura. but increasingly, durga puja is also the world’s largest publicly funded Art show. </p>
+            </div>
         </div>
     );
 };
