387d6cf5c77840b702ef4a72ff623c95ca6a7d06
himaloyash <himaloyashis98@gmail.com>
2024-06-21 18:45:24 +0600

    second section is on

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -4,8 +4,7 @@ import './Banner.css'
 const Banner = () => {
     return (
         <div className=' bannerBg '>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eligendi illo fuga dolor alias est nisi quidem veniam sed aliquid vitae commodi accusamus at possimus placeat ullam laborum rem, consectetur quos magnam non! Quis voluptas delectus ea iusto harum tenetur quam minima officiis distinctio, autem laboriosam, beatae aperiam molestias. Laboriosam facere inventore beatae ullam fuga deleniti itaque placeat quos perferendis? Ipsam, inventore placeat. Enim impedit maiores saepe ab, repudiandae, molestias minus possimus corporis non quaerat illo inventore aliquam numquam adipisci cum quas ad. Sequi harum suscipit vel laudantium beatae aut deleniti, facilis architecto repudiandae explicabo fuga, corporis unde ipsa voluptatibus?</p>
-            
+            <p className='  '>Witness The World’s Biggest</p>
         </div>
     );
 };
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
-import './Navbar'
+import React from "react";
+import "./Navbar";
 
 const Navbar = () => {
-
-    return (
-        <div className='flex justify-between pl-[120px] bg-black pt-5  pr-[120px]  '>
-            <ul>
-                <li><img src="/src/assets/massart 1.png" alt="MassArt" /> </li>
-            </ul>
-            <ul className='flex gap-3'>
-                <li className='text-white'>GetTicket</li>
-                <li className='text-white' >Schedules</li>
-                <li className='text-white' >Speakers</li>
-                <li className='text-white' >FAQ</li>
-            </ul>
-            <ul><li><button className='text-white' >Register</button></li></ul>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-between pl-[120px] bg-black pt-5  pr-[120px]  ">
+      <div>
+        <li>
+          <img src="/src/assets/massart 1.png" alt="MassArt" />{" "}
+        </li>
+      </div>
+      <div className="flex gap-3">
+        <a href="GetTicket" className="text-white">
+          GetTicket
+        </a>
+        <a href="Schedules" className="text-white">
+          Schedules
+        </a>
+        <a href=" Speakers" className="text-white">
+          Speakers
+        </a>
+        <a href="FAQ " className="text-white">
+          FAQ
+        </a>
+      </div>
+      <div>
+        <button className="text-white bg-r bg-[#fc032c] w-[100px] h-8 rounded-2xl ">Register</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
