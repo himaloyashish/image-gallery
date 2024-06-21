5d344cc2f96ec12a821c763fee650cb4aa0a3440
himaloyash <himaloyashis98@gmail.com>
2024-06-22 00:18:37 +0600

    gallery is on now

--- src/Founder/Founder.jsx
+++ src/Founder/Founder.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Founder = () => {
     return (
-        <div className='bg-[#212525] pl-[120px] pr-[120px] pt-[150]'>
-            <p>Lorem Ipsum Is Simply Dummy</p>
-            <div>
-                <img src="/src/assets/Rectangle- thire 1.png" alt="" />
-                <img src="/src/assets/Rectangle- Third 2.png" alt="" />
-                <img src="/src/assets/Rectangle- Third 3.png" alt="" />
-                <img src="/src/assets/Rectangle- Third 4.png" alt="" />
+        <div className='bg-[#212525] pl-[120px] pr-[120px] pt-[150px]'>
+            <p className='text-4xl font-bold w-[480px] m-auto pb-4 text-white'>Lorem Ipsum Is Simply Dummy</p>
+            <div className='md:flex'>
+                <img src="/src/assets/sec third 1.png" alt="" />
+                <img src="/src/assets/god 1.png" alt="" />
+                <img src="/src/assets/Rectangle- second 3.png" alt="" />
+                
                 
             </div>
 
