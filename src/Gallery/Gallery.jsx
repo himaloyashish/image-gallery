dde45384874d2250edfbe8006d7639997b8103d5
himaloyash <himaloyashis98@gmail.com>
2024-06-24 09:27:52 +0600

    again image size

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -16,7 +16,7 @@ const Gallery = () => {
       <div className="md:flex  justify-between items-center gap-3">
         <div className="gap-3 w-2/2">
           <img className="object-fill " src="https://i.ibb.co/RDdcTwF/Rectangle-thire-1.png" alt="" />
-          <div className="md:flex justify-between items-center gap-2 pt-5">
+          <div className="md:flex w-2/2 justify-between items-center gap-2 pt-5">
             <img className="object-fill" src="https://i.ibb.co/Csyg3p9/Rectangle-Third-2.png" alt="" />
             <img className="object-fill" src="https://i.ibb.co/Wp4Xm3J/Rectangle-third-3.png" alt="" />
           </div>
